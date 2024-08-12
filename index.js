15bcf409eb69efe6b3371d25d10f2b2a490ca334
Chris <chris79gp@outlook.com>
2024-08-12 20:59:12 +0100

    feat: add error handling to `server`

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
 	const file = req.url.replace(/\//g, "") || "index";
-	fs.readFile(`${file}.html`, (e, data) => {
-		res.writeHead(200, { "Content-Type": "text/html" });
-		res.write(data);
-		return res.end();
+
+	fs.readFile(`${file}.html`, (err, data) => {
+		if (err) {
+			res.writeHead(404, { "Content-Type": "text/plain" });
+			res.end("404 Not Found");
+		} else {
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(data);
+		}
 	});
 });
 
