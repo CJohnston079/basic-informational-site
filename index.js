5aed876f5dc4ad537258a6c34e6cca86012bf07b
Chris <chris79gp@outlook.com>
2024-08-12 21:11:34 +0100

    feat: serve `404.html` where endpoint is a non-existent file

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	const file = req.url.replace(/\//g, "") || "index";
+	const fileName = req.url.replace(/\//g, "") || "index";
+	const filePath = `./${fileName}.html`;
 
-	fs.readFile(`${file}.html`, (err, data) => {
+	fs.readFile(filePath, (err, data) => {
 		if (err) {
-			res.writeHead(404, { "Content-Type": "text/plain" });
-			res.end("404 Not Found");
+			fs.readFile("./404.html", (err, data) => {
+				if (err) {
+					res.writeHead(404, { "Content-Type": "text/plain" });
+					res.end("404 Not Found");
+				} else {
+					res.writeHead(404, { "Content-Type": "text/html" });
+					res.end(data);
+				}
+			});
 		} else {
 			res.writeHead(200, { "Content-Type": "text/html" });
 			res.end(data);
